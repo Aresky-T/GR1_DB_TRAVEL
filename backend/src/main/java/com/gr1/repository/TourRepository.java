e77bf5727da16f726c4185f95ccea55e1af3e586
Aresky-T <tn6354103@gmail.com>
2023-05-18 17:56:31 +0700

    fix TourRepository_backend

--- backend/src/main/java/com/gr1/repository/TourRepository.java
+++ backend/src/main/java/com/gr1/repository/TourRepository.java
@@ -1,8 +1,9 @@
 package com.gr1.repository;
 
+import com.gr1.entity.Tour;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TourRepository extends JpaRepository<TourRepository, Integer> {
+public interface TourRepository extends JpaRepository<Tour, Integer> {
 }
