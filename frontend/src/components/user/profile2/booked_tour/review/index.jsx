4dbb79d6d4e1ec6dde20a1a0ab5868a8e0ca4d89
Aresky-T <tn6354103@gmail.com>
2024-01-17 11:40:36 +0700

    update ReviewBookedTour component

--- frontend/src/components/user/profile2/booked_tour/review/index.jsx
+++ frontend/src/components/user/profile2/booked_tour/review/index.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import "./style.scss";
 
-const ReviewBookedTour = () => {
-  return <div className="review-booked-tour">review</div>;
+const ReviewBookedTour = ({bookedTour}) => {
+  const [review, setReview] = useState({
+    isShowModal: false,
+    stars: 0,
+    comment: '',
+    tourId: null,
+  });
+
+  return <div className="review-booked-tour modal">review</div>;
 };
 
 export default ReviewBookedTour;
